a6add4a0dca2fbdf10d13287256d92bbb6ba744a
austin vargason <vargasona@austins-MacBook-Pro.local>
2018-05-11 20:20:53 -0700

    added Main.java.

---
+++ src/vargason_austin/Main.java
@@ -0,0 +1,69 @@
+/*
+Name: Main.java
+Description: Main file for FinalStudy Project
+Author: Austin Vargason
+Date Modified: 5/11/18
+ */
+
+//vargason_austin package
+package vargason_austin;
+
+//imports for Main
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Scene mainScene = getMainScene();
+        primaryStage.setTitle("Test Form");//set the title
+        primaryStage.setScene(mainScene);//set the scene
+        primaryStage.show();//show the window
+    }
+
+    //method to get Main Scene for project
+    public static Scene getMainScene() {
+        //gridPane to store items in form
+        GridPane gridPane = new GridPane();
+
+        //items to add into GridPane
+        TextField firstNameField =  new TextField();
+        TextField lastNameField = new TextField();
+        TextField emailField = new TextField();
+        Button submitButton = new Button("Submit");
+
+        //add items to the GridPane
+        gridPane.add(new Label("First Name: "), 0, 0);
+        gridPane.add(firstNameField, 1, 0);
+        gridPane.add(new Label("Last Name: "), 0, 1);
+        gridPane.add(lastNameField, 1, 1);
+        gridPane.add(new Label("Email: "), 0, 2);
+        gridPane.add(emailField, 1, 2);
+        gridPane.add(submitButton, 1, 3);
+
+        //set GridPane options
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setHgap(10);
+        gridPane.setVgap(5);
+        GridPane.setHalignment(submitButton, HPos.RIGHT);
+
+        //scene to return
+        Scene mainScene = new Scene(gridPane, 300, 200);
+
+        //return the scene
+        return mainScene;
+    }
+
+    //main method used for testing inside of IDE
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
